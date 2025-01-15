04b9c80310239c956b9a3eb23eca5d6ef1f90e27
sanskar bohora <114263649+sanskarbohora@users.noreply.github.com>
2025-01-15 17:10:53 +0530

    added class E & F

---
+++ QAMPA20241012/java_intro/src/access_specifiers/F.java
@@ -0,0 +1,31 @@
+package access_specifiers;
+
+public class F {
+	private int a = 10;
+	protected int b = 20;
+	int c = 10;
+	public int d = 20;
+	public void test() {
+		System.out.println("Test A");
+
+	}
+
+	protected void testPro() {
+		System.out.println("TestPro A");
+	}
+
+	void testV() {
+		System.out.println("TestV A");
+	}
+
+	private void testPri() {
+		System.out.println("TestPri A");
+	}
+
+	public static void main(String[] args) {
+		F a = new F();
+		a.testPri();
+		System.out.println(a.a);
+
+	}
+}
